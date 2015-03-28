c23f326821d433ace1a4e933aa751413798ba817
shenchao <shenchao890216@163.com>
2015-03-29 00:08:22 +0800

    建立项目

---
+++ gotop.js
@@ -0,0 +1,17 @@
+/**
+ *  Copyright (c) 2015 Scrd (https://github.com/shenchao890216)
+ *  Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ *
+ *  Version 0.0.1(Beta)
+ */
+
+(function($) {
+	$.fn.gotop = function(options) {
+		// 默认参数.
+		var defaults = {};
+		// 合并用户参数.
+		var options = $.extend(defaults, options);
+		// 功能实现.
+		this.each(function() {});
+	};
+})(jQuery);
