18b9658ff7c01a7cc98ef98a582f02def8c7cbf2
shenchao <shenchao890216@163.com>
2015-03-29 23:27:02 +0800

    完成默认返回图标的制作

--- gotop.js
+++ gotop.js
@@ -23,31 +23,37 @@
 		// 合并参数.
 		var options = $.extend(defaults, options);
 		// 添加gotop元素.
-		$('body').append('<div class="' + options.containerClass + '" style="none"></div>');
+		$('body').append('<div class="' + options.containerClass + '"></div>');
 		var $gotopContainer = $('.' + options.containerClass);
 		$gotopContainer.html('<div class="' + options.arrowClass + '"></div>');
-		var gotopArrow = $('.' + options.arrowClass);
+		var $gotopArrow = $('.' + options.arrowClass);
 		// gotop包裹元素的css样式.
 		var containerStyle = {
 			'position': 'fixed',
-			options.location: options.locationOffset + 'px',
+			// options.location: options.locationOffset + 'px',
 			'bottom': options.bottomOffset + 'px',
 			'width': options.containerSize + 'px',
 			'height': options.containerSize + 'px',
 			'background-color': options.containerColor,
 			'cursor': 'pointer',
-			'border-radius': containerRadius
+			'border-radius': options.containerRadius
 		};
+		containerStyle[options.location] = options.locationOffset + 'px';
 		
 		$gotopContainer.css(containerStyle);
 		// 箭头的样式.
-		var arrowSize = 0.25 * options.containerSize,
+		var borderSize = 0.25 * options.containerSize,
 			arrowStyle = {
 				'width': 0,
 				'height': 0,
+				'padding-top': ((options.containerSize - 3 * borderSize) / 2) + 'px',
 				'margin': '0 auto',
-				
+				'border-style': 'solid',
+				'border-width': borderSize + 'px',
+				'border-color': 'transparent transparent ' + options.arrowColor + ' transparent'
 			};
+		// 设置箭头的css.
+		$gotopArrow.css(arrowStyle);
 	};
 
 	// 元素实现，用户自己定制样式和结构.
